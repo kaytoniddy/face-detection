6d691e76ed413071634f333353ed69a8d103a5b6
kayton <kayton.chiu@iddy.ai>
2024-01-24 14:27:56 +0800

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,84 @@
-import Image from 'next/image'
+'use client'
+
+import { useRef, useState } from "react";
+import * as faceapi from "face-api.js";
+
+const MODEL_URL = './models';
 
 export default function Home() {
+
+  // Create a reference to the hidden file input element
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+  const imgElementRef = useRef<HTMLImageElement>(null);
+  const [isDetecting, setIsDetecting] = useState(false);
+  const [noOfFaces, setNoOfFaces] = useState<faceapi.FaceDetection[]>([]);
+
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
+  const handleClick = (_: any) => {
+    hiddenFileInput.current?.click();
+  };
+
+  // Call a function (passed as a prop from the parent component)
+  // to handle the user-selected file 
+  const handleChange = async (event: any) => {
+    const fileUploaded = event.target.files[0];
+    const imgElement = imgElementRef.current;
+    if (imgElement) {
+      imgElement.src = URL.createObjectURL(fileUploaded);
+    }
+  };
+  
+  const detectFace = async () => {
+    if (!imgElementRef.current || isDetecting) return;
+    setIsDetecting(true);
+    /** load the model from public or url. right now it is using the model from <root-dir>/public/models/xxx */
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+    // validate the uploaded image here
+    /** detectAllFaces requires a HTMLImageElement */
+    const res = await faceapi.detectAllFaces(imgElementRef.current!);
+    setIsDetecting(false);
+    setNoOfFaces(res);
+
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm flex gap-2">
+        <div style={{width: 600, height: 600}} className='border-2'>
+          <img ref={imgElementRef} className='w-full' />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col">
+          <div className="flex gap-4">
+            <div>
+              <button onClick={handleClick} className="p-2 rounded bg-blue-400 text-white">upload</button>
+              <input
+                type="file"
+                onChange={handleChange}
+                ref={hiddenFileInput}
+                style={{ display: 'none' }} // Make the file input element invisible
+              />
+            </div>
+            <div>
+              <button disabled={isDetecting} onClick={detectFace} className="p-2 rounded bg-blue-400 text-white">{isDetecting ? "loading...": "detect face"}</button>
+            </div>
+          </div>
+          <div>
+            <p>no of faces: {noOfFaces.length}</p>
+            {
+              noOfFaces.map((face, index) => {
+                return (
+                  <div key={index}>
+                    <p>face {index + 1}</p>
+                    <p>confidence: {face.score}</p>
+                    {/* <p>box: {face.detection._box.x}, {face.detection._box.y}, {face.detection._box.width}, {face.detection._box.height}</p> */}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
     </main>
   )
